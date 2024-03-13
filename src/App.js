514273259ddc40b913e15ef546c90be96a7dc73f
김대성 <daesung3725@naver.com>
2024-03-13 18:00:15 +0900

    fix: chatJoin 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ChatRoom from './ChatRoom/ChatRoom';
-import ChatJoin from './ChatJoin';
+import ChatJoin from './Chat/ChatJoin';
 
 function App() {
   const [nickname, setNickname] = useState("");
@@ -14,7 +14,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" exact={true} element={<ChatJoin onJoin={handleJoin} />} />
-        {/* prop 이름을 nickname으로 수정 */}
         <Route path='/chatRoom/:nickname' element={<ChatRoom nickname={nickname}/>} /> 
       </Routes>
     </div>
--- src/Chat/ChatJoin.jsx
+++ src/Chat/ChatJoin.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import './ChatJoin.scss';
 const ChatJoin = () => {
     const [nickname, setNickname] = useState('');
     const navigate = useNavigate();
@@ -9,17 +10,21 @@ const ChatJoin = () => {
         navigate(`/chatRoom/${nickname}`);
     }
   return (
-    <div>
-        <h1>ChatJoin</h1>
-        <form onSubmit={handleSubmit}>
-            <input 
+    <div className='chatJoin'>
+      <div className='chatJoin__container'>
+        <form onSubmit={handleSubmit} className='chatJoin__container__form'>
+          <h1>ChatJoin</h1>
+          <p>오픈채팅방에서 사용하실 닉네임을 입력하세요</p>
+          <input 
+            className='chatJoin__inputBox'
             type='text' 
             placeholder='닉네임을 입력 하세요' 
             value={nickname}
-            onChange={(e)=>setNickname(e.target.value)}
-            />
-            <button type='submit'>입장하기</button>
+            onChange={(e)=>setNickname(e.target.value)}            
+          />
+          <button type='submit'>입장하기</button>
         </form>
+      </div>
     </div>
   )
 }
