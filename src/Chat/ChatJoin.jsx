47b25340c7e138ed828518cdb25083217ac3dcab
김대성 <daesung3725@naver.com>
2024-03-24 17:18:10 +0900

    feat: join시 post 하는 코드 추가

--- src/Chat/ChatJoin.jsx
+++ src/Chat/ChatJoin.jsx
@@ -1,17 +1,32 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import './ChatJoin.scss';
+import axios from 'axios';
 const ChatJoin = () => {
     const [userName, setUserName] = useState('');
     const [roomName, setRoomName] = useState('');
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
-        if(userName.trim() === '' || roomName.trim() === ''){ // 'trim()' 메소드는 문자열의 양 끝에 있는 공백을 제거합니다.
-          alert("닉네임 또는 채팅 룸 번호가 입력되지 않았습니다.");
-        }else{
-          navigate(`/chatRoom/${roomName}/${userName}`);
+      event.preventDefault(); //이벤트의 기본 동작을 방지합니다.
+      if(userName.trim() === '' || roomName.trim() === ''){ // 'trim()' 메소드는 문자열의 양 끝에 있는 공백을 제거합니다.
+        alert("닉네임 또는 채팅 룸 번호가 입력되지 않았습니다.");
+      }else{
+        const JoinData = {
+          userName:userName,
+          roomName:roomName
         }
+        //axios를 사용하여 POST 요청을 보냅니다. 
+        axios.post("http://kim-sun-woo.com/chat/info", JoinData)
+          .then(response =>{
+            //요청이 성공한 경우, ChatRoom으로 이동합니다. 
+            navigate(`/chatRoom/${roomName}/${userName}`);
+          })
+          .catch(error => {
+              console.error('Error sending data to server:', error);
+              alert("서버와의 통신에 문제가 발생했습니다.");
+          })
+      }
     }
     const handleOnKeyPress = (event) => {
       if (event.keyCode === 13) {
