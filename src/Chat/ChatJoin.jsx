d066ec968ef380eedc00cc3bd20f4ad837281f0d
김대성 <daesung3725@naver.com>
2024-03-25 09:43:28 +0900

    feat: 미디어쿼리 적용

--- src/Chat/ChatJoin.jsx
+++ src/Chat/ChatJoin.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import './ChatJoin.scss';
-import axios from 'axios';
+// import axios from 'axios';
 const ChatJoin = () => {
     const [userName, setUserName] = useState('');
     const [roomName, setRoomName] = useState('');
@@ -12,26 +12,27 @@ const ChatJoin = () => {
       if(userName.trim() === '' || roomName.trim() === ''){ // 'trim()' 메소드는 문자열의 양 끝에 있는 공백을 제거합니다.
         alert("닉네임 또는 채팅 룸 번호가 입력되지 않았습니다.");
       }else{
-        const JoinData = {
-          userName:userName,
-          roomName:roomName
-        }
-        //axios를 사용하여 POST 요청을 보냅니다. 
-        axios.post("http://kim-sun-woo.com/chat/info", JoinData)
-          .then(response =>{
-            //요청이 성공한 경우, ChatRoom으로 이동합니다. 
-            navigate(`/chatRoom/${roomName}/${userName}`);
-          })
-          .catch(error => {
-              console.error('Error sending data to server:', error);
-              alert("서버와의 통신에 문제가 발생했습니다.");
-          })
+        navigate(`/chatRoom/${roomName}/${userName}`);
+        // const JoinData = {
+        //   userName:userName,
+        //   roomName:roomName
+        // }
+        // //axios를 사용하여 POST 요청을 보냅니다. 
+        // axios.post("http://kim-sun-woo.com/chat/info", JoinData)
+        //   .then(response =>{
+        //     //요청이 성공한 경우, ChatRoom으로 이동합니다. 
+        //     navigate(`/chatRoom/${roomName}/${userName}`);
+        //   })
+        //   .catch(error => {
+        //       console.error('Error sending data to server:', error);
+        //       alert("서버와의 통신에 문제가 발생했습니다.");
+        //   })
       }
     }
     const handleOnKeyPress = (event) => {
       if (event.keyCode === 13) {
         event.preventDefault();
-        handleSubmit(); // Enter 입력이 되면 클릭 이벤트 실행
+        handleSubmit(event); // Enter 입력이 되면 클릭 이벤트 실행
       }
     };
   return (
